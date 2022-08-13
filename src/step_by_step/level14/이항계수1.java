8287d7f7436f6233ddf9841435142d6d95150974
lhj0954 <leehakjun111@naver.com>
2022-08-13 15:21:14 +0900

    이항계수2

--- src/step_by_step/level14/이항계수1.java
+++ src/step_by_step/level14/이항계수1.java
@@ -4,7 +4,11 @@ import java.util.*;
 
 public class 이항계수1 {
     static int factorial (int n) {
-        if ()
+        if (n == 1) {
+            return 1;
+        } else {
+            return n*factorial(n-1);
+        }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -12,12 +16,6 @@ public class 이항계수1 {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        if (m == 0) {
-            System.out.println(1);
-        } else if (n == m) {
-            System.out.println(1);
-        } else {
-            System.out.println(n * (n - 1) / m);
-        }
+        System.out.println(factorial(n)/(factorial(m)*factorial(n-m)));
     }
 }
---
+++ src/step_by_step/level14/이항계수2.java
@@ -0,0 +1,19 @@
+package step_by_step.level14;
+
+import java.util.*;
+
+public class 이항계수2 {
+    static int Pascal (int n, int m) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return Pascal();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
